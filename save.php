ec13ab8ce70e801a87969a53c5f75cc79465e1e3
gwg <ggundersen@gmail.com>
2015-12-13 20:33:05 -0500

    db.conf for database connection

--- save.php
+++ save.php
@@ -1,14 +1,14 @@
 <?php
 
 // Variables
-$func	= $_GET["func"];
-$sortby	= $_GET["sortby"];
-$order	= $_GET["order"];
-$getID	= $_GET["id"];
+//$func	= $_GET["func"];
+//$sortby	= $_GET["sortby"];
+//$order	= $_GET["order"];
+//$getID	= $_GET["id"];
 
 // submit
 // for error submission error messages, see /js/slate.js
-if ($_POST["category"] && $_POST["cost"]) {
+/*if ($_POST["category"] && $_POST["cost"]) {
 	
 	$cost 	  = $_POST["cost"];
 	$category = $_POST["category"];
@@ -19,6 +19,16 @@ if ($_POST["category"] && $_POST["cost"]) {
 	$query .= $category . "','";
 	$query .= $comment . "')";
 	mysql_query($query);
+}*/
+
+$fh = fopen('db.conf','r');
+while ($line = fgets($fh)) {
+    echo($line);
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    var_dump($_POST['cost']);
 }
 
 mysql_close($conn);
