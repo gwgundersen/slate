cc8a5af5d25e8ed669559bb221cca9fd0bdef766
gwg <ggundersen@gmail.com>
2015-12-15 09:20:12 -0500

    View report page

--- index.php
+++ index.php
@@ -4,39 +4,47 @@
 	<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
 	<meta content='width=device-width, initial-scale=1' name='viewport'/>
 	<title>Slate</title>
-    <link rel='stylesheet' href='main.css'/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>
+    <link rel='stylesheet' href='main.css'/>
     <script src='main.js'></script>
 </head>
 <body>
-<div class='page'>
+<div class='container'>
     <h1>Slate</h1>
-    <form>
-        <div>
-            <input name='cost' type='text' placeholder='Cost ($)' autocomplete='off'/>
+    <div class='row'>
+        <div class='col-sm-3'>
+            <form>
+                <div class='form-group'>
+                    <input class='form-control' name='cost' type='text' placeholder='Cost ($)' autocomplete='off'/>
+                </div>
+                <div class='form-group'>
+                    <select class='form-control' name='category' autocomplete='off'>
+                        <option value='select'>(Category)</option>
+                        <option value='food-in'>Food (In)</option>
+                        <option value='food-out'>Food (Out)</option>
+                        <option value='alcohol'>Alcohol</option>
+                        <option value='household'>Household items</option>
+                        <option value='clothing'>Clothing</option>
+                        <option value='transportation'>Transportation</option>
+                        <option value='miscellaneous'>Miscellaneous</option>
+                    </select>
+                </div>
+                <div class='form-group'>
+                    <input class='form-control' name='comment' type='text' placeholder='Comment' autocomplete='off'/>
+                </div>
+                <div class='form-group'>
+                    <input class='form-control' id='pw' type='text' placeholder='Password'/>
+                </div>
+                <div class='form-group'>
+                    <button class='form-control' class='btn btn-default'>Add</button>
+                </div>
+            </form>
         </div>
-        <div>
-            <select name='category' autocomplete='off'>
-                <option value='select'>(Category)</option>
-                <option value='food-in'>Food (In)</option>
-                <option value='food-out'>Food (Out)</option>
-                <option value='alcohol'>Alcohol</option>
-                <option value='household'>Household items</option>
-                <option value='clothing'>Clothing</option>
-                <option value='transportation'>Transportation</option>
-                <option value='miscellaneous'>Miscellaneous</option>
-            </select>
+        <div class='col-sm-3'>
+            <a href='/slate/view.php'>View Report</a>
         </div>
-        <div>
-            <input name='comment' type='text' placeholder='Comment' autocomplete='off'/>
-        </div>
-        <div>
-            <input id='pw' type='text' placeholder='Password'/>
-        </div>
-        <div>
-            <button>Add</button>
-        </div>
-    </form>
+    </div>
 </div>
 </body>
 </html>
