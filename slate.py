47dece767bed02fd5bbefe1dc22d6b72231707d5
= <ggundersen@gmail.com>
2016-01-01 19:19:24 -0700

    Converting PHP app to Python app

---
+++ slate.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/the-time')
+def the_time():
+     cur_time = str(datetime.now())
+     return cur_time + ' is the current time!  ...YEAH!'
