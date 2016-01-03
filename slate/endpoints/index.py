ec1a593e56e6fa0385dd5c0f94693800c7b2ab87
gwg <ggundersen@gmail.com>
2016-01-03 00:13:02 -0500

    Cleanup

--- slate/endpoints/index.py
+++ slate/endpoints/index.py
@@ -6,6 +6,7 @@ from flask import Blueprint, render_template
 from flask.ext.login import current_user
 import MySQLdb
 
+from slate import db
 from slate.config import config
 
 
@@ -16,9 +17,12 @@ index = Blueprint('index',
 
 @index.route('/', methods=['GET'])
 def index_page():
+    categories = db.get_categories()
     if current_user.is_authenticated:
         auth_message = '%s is logged in.' % current_user.name
     else:
         auth_message = 'No user logged in.'
-    return render_template('index.html', auth_message=auth_message)
+    return render_template('index.html',
+                           categories=categories,
+                           auth_message=auth_message)
 
