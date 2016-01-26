4a99cc8573bc1d99b4dfa0bce733cf3cd20b878f
gwg <ggundersen@gmail.com>
2016-01-26 00:48:49 -0500

    Removing commented out code

--- slate/static/js/report.js
+++ slate/static/js/report.js
@@ -42,18 +42,6 @@ window.plotExpensesTimeSeries = function(perDayData) {
         },
         plotOptions: {
             area: {
-                //fillColor: {
-                //    linearGradient: {
-                //        x1: 0,
-                //        y1: 0,
-                //        x2: 0,
-                //        y2: 1
-                //    },
-                //    stops: [
-                //        [0, Highcharts.getOptions().colors[0]],
-                //        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                //    ]
-                //},
                 marker: {
                     radius: 2
                 },
