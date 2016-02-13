590ffcb64e1a01d1e9cae21467f457482f99fb03
gwg <ggundersen@gmail.com>
2016-02-13 09:34:23 -0500

    Fixed SQL script to run over all existing users and clarified names

--- schema/20160211_migration.sql
+++ schema/20160211_migration.sql
@@ -1,7 +1,5 @@
-ALTER TABLE `category` ADD COLUMN user_fk INT(11) DEFAULT 1
-AFTER name;
-ALTER TABLE `category` ADD COLUMN hide_in_report BOOL DEFAULT FALSE
-AFTER user_fk;
+ALTER TABLE `category` ADD COLUMN `user_fk` INT(11) DEFAULT 1 AFTER `name`;
+ALTER TABLE `category` ADD COLUMN `hide_in_report` BOOL DEFAULT FALSE AFTER `user_fk`;
 
 DROP PROCEDURE IF EXISTS update_categories;
 
@@ -9,57 +7,57 @@ DELIMITER #
 CREATE PROCEDURE update_categories()
   BEGIN
 
-    DECLARE user_fk INT UNSIGNED DEFAULT 1;
+    DECLARE user_id INT UNSIGNED DEFAULT 1;
 
     START TRANSACTION;
-    loop_label: WHILE user_fk < 10 DO
+    loop_label: WHILE user_id < 15 DO
 
-      SET user_fk = user_fk + 1;
+      SET user_id = user_id + 1;
 
-      IF user_fk = 5
+      IF user_id = 5
       THEN
         ITERATE loop_label;
       END IF;
-      IF user_fk = 9
+      IF user_id = 9
       THEN
         ITERATE loop_label;
       END IF;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("alcohol", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 3 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("alcohol", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 3 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("bills", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 6 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("bills", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 6 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("clothing", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 13 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("clothing", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 13 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("entertainment", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 10 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("entertainment", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 10 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("food (in)", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 1 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("food (in)", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 1 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("food (out)", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 2 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("food (out)", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 2 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("household", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 8 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("household", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 8 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("medical", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 11 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("medical", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 11 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("miscellaneous", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 7 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("miscellaneous", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 7 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("rent/mortgage", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 5 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("rent/mortgage", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 5 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("transportation", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 4 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("transportation", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 4 AND `user_fk` = user_id;
 
-      INSERT INTO `category` (`name`, `user_fk`) VALUES ("travel/vacation", user_fk);
-      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 9 AND `user_fk` = user_fk;
+      INSERT INTO `category` (`name`, `user_fk`) VALUES ("travel/vacation", user_id);
+      UPDATE `expense` SET `category_fk` = (SELECT LAST_INSERT_ID()) WHERE `category_fk` = 9 AND `user_fk` = user_id;
 
     END WHILE loop_label;
     COMMIT;
